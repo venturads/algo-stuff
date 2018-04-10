cadc316af09985c093276923233ab64baff7a354
Albert Ventura <albert@venturads.com>
2018-04-10 23:24:32 +0000

    finished form event listener

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myFm = function(){
     let nm = document.getElementById("fname").value;
-    
+    document.getElementById("demo").innerHTML = "Welcome: " + nm;
 }
 document.getElementById("btn").addEventListener("click", myFm);
